7a4a709b02db8dbc09cb258a6d83718b8bdd1ad8
Michael Souto <michael.souto@detrasoft.com>
2025-02-16 19:02:08 -0300

    Implementando melhorias nos componentes da framework

--- src/lib/models/config-system.model.ts
+++ src/lib/models/config-system.model.ts
@@ -4,7 +4,9 @@ import { UtilsService } from "projects/design-lib/src/lib/services/utils/utils.s
 export class ConfigSystem extends GenericEntity {
   code?: string;
   name?: string;
+  feature?: string;
   config?: string;
+  filter?: string;
   type?: TypeConfig;
 }
 
@@ -12,14 +14,18 @@ export function newConfigSystem(
   id?: string,
   code?: string,
   name?: string,
+  feature?: string,
   config?: string,
+  filter?: string,
   type?: TypeConfig
 ): ConfigSystem {
   const configSystem = new ConfigSystem();
   configSystem.id = id;
   configSystem.code = code;
   configSystem.name = name;
+  configSystem.feature = feature
   configSystem.config = config;
+  configSystem.filter = filter;
   configSystem.type = type;
   return configSystem;
 }
@@ -33,7 +39,7 @@ export const SESSION = 'SESSION';
 export const LOGIN = 'LOGIN';
 export const PASS = 'PASS';
 
-export let CONFIG_SESSION: ConfigSystem = newConfigSystem(UtilsService.generateUUID(), SESSION, 'User session', null, TypeConfig.LOCAL_CONFIG);
-export let LOGIN_SESSION: ConfigSystem = newConfigSystem(UtilsService.generateUUID(), LOGIN, 'Login session', null, TypeConfig.LOCAL_CONFIG);
-export let PASS_SESSION: ConfigSystem = newConfigSystem(UtilsService.generateUUID(), PASS, 'Pass session', null, TypeConfig.LOCAL_CONFIG);
+export let CONFIG_SESSION: ConfigSystem = newConfigSystem(UtilsService.generateUUID(), SESSION, 'User session', 'auth', null, null, TypeConfig.LOCAL_CONFIG);
+export let LOGIN_SESSION: ConfigSystem = newConfigSystem(UtilsService.generateUUID(), LOGIN, 'Login session', 'auth',null, null, TypeConfig.LOCAL_CONFIG);
+export let PASS_SESSION: ConfigSystem = newConfigSystem(UtilsService.generateUUID(), PASS, 'Pass session', 'auth',null, null, TypeConfig.LOCAL_CONFIG);
 
